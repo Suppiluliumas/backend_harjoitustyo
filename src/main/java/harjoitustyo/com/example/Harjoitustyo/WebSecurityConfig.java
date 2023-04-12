ec985c55da488428e5e46c2424d2282fdaad9ca9
Jesse N <jesse.nevalainen@myy.haaga-helia.fi>
2023-04-12 22:10:51 +0300

    add user functionability, starting to change bootstrap

--- src/main/java/harjoitustyo/com/example/Harjoitustyo/WebSecurityConfig.java
+++ src/main/java/harjoitustyo/com/example/Harjoitustyo/WebSecurityConfig.java
@@ -10,41 +10,28 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
+
 @Configuration
 @EnableMethodSecurity(securedEnabled = true)
 public class WebSecurityConfig {
-   @Autowired
-   private UserDetailsService userDetailsService;
-	
-	@Bean
-    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-        http
-        .authorizeHttpRequests()
-        	.requestMatchers("/css/**").permitAll() // Enable css when logged out
-        	.requestMatchers("/delete/{id}").hasAuthority("ADMIN")
-        	.requestMatchers("/editgame/{id}").hasAuthority("ADMIN")
-        	.requestMatchers(toH2Console()).permitAll()
-        	.anyRequest().authenticated()
-        	.and()
-        	.csrf().ignoringRequestMatchers(toH2Console())
-      	  	.and()
-      	  	.headers().frameOptions().disable()
-      	  	.and()
-      .formLogin()
-      		.defaultSuccessUrl("/gamelist", true)
-          .permitAll()
-          .and()
-      .logout()
-          .permitAll()
-          .and()
-      .httpBasic();
-      return http.build();
-    }
-    
+	@Autowired
+	private UserDetailsService userDetailsService;
 
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
-    }
+	@Bean
+	public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests().requestMatchers("/css/**").permitAll() // Enable css when logged out
+				.requestMatchers("/").permitAll().requestMatchers("/gamelist").permitAll()
+				.requestMatchers("/delete/{id}").hasAuthority("ADMIN").requestMatchers("/editgame/{id}")
+				.hasAuthority("ADMIN").requestMatchers(toH2Console()).permitAll().anyRequest().authenticated().and()
+				.csrf().ignoringRequestMatchers(toH2Console()).and().headers().frameOptions().disable().and()
+				.formLogin().loginPage("/login").defaultSuccessUrl("/gamelist", true).permitAll().and().logout()
+				.permitAll().and().httpBasic();
+		return http.build();
+	}
+
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
+	}
 
 }
